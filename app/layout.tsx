8f9b946ee63e9e032f217c9ad252ad6db45d2547
Ammar1122s <ammaraamir33@gmail.com>
2024-03-01 19:05:52 +0500

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const ibm = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -20,8 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider appearance={{
+      variables:{ colorPrimary: "#624cf6"}
+    }}>
     <html lang="en">
       <body className={cn("font-ibm antialiased", ibm.variable)}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
